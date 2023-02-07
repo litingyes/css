0bc53091320f1eddbf4ad065c4fd8f038fa12624
liting <luz.liting@gmail.com>
2023-02-07 13:02:33 +0800

    chore(config): init site config

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,7 +2,25 @@ import { defineConfig } from 'vitepress'
 import MarkdownPreview from 'vite-plugin-markdown-preview'
 
 export default defineConfig({
+  title: 'CSS World',
+  description: 'A css knowledge collection',
+  lang: 'en-US',
+  lastUpdated: true,
+  cleanUrls: true,
   vite: {
     plugins: [MarkdownPreview()],
+  },
+  themeConfig: {
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/liting-yes/css' },
+    ],
+    footer: {
+      message: 'Released under the MIT License.',
+      copyright: 'Copyright © 2023-present Liting Yes',
+    },
+    editLink: {
+      pattern: 'https://github.com/liting-yes/css/edit/main/docs/:path',
+      text: 'Edit this page on GitHub',
+    },
   },
 })
