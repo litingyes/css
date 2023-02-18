8476abab211eb21cdbf83adb4217389d517dcf34
Lovell Liu <lovelliu@foxmail.com>
2023-02-18 11:41:28 +0800

    feat: add juejin-code link for ripple animation

--- docs/.vitepress/theme/utils/link.ts
+++ docs/.vitepress/theme/utils/link.ts
@@ -7,6 +7,7 @@ export const juejinLinks: { [key: string]: string } = {
   '/demo/animation/horse-racing-light': 'https://code.juejin.cn/pen/7200239145396994104',
   '/demo/animation/spin': 'https://code.juejin.cn/pen/7199300128362987535',
   '/demo/animation/typewriter': 'https://code.juejin.cn/pen/7198537369425821735',
+  '/demo/animation/ripple': 'https://code.juejin.cn/pen/7201334156121341989',
   '/demo/interact/dynamic-background-color': 'https://code.juejin.cn/pen/7199291750588874807',
   '/demo/interact/dynamic-border': 'https://code.juejin.cn/pen/7199292584592670779',
   '/demo/interact/neon': 'https://code.juejin.cn/pen/7198536565709078580',
