c50ddbff560e0988432160dacacc40e6c53ff096
liting <luz.liting@gmail.com>
2023-03-08 21:10:56 +0800

    refactor: split config of sidebar

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vitepress'
+import { sidebarEn, sidebarZh } from './config/sidebar'
 
 const ogImage = 'https://og.lovelliu.me/api/og?author=CSS%20Planet&backgroundImage=https%3A%2F%2Fres.craft.do%2Fuser%2Ffull%2Fa00fc09b-5dd0-bc21-aaeb-f7e491dce279%2Fdoc%2F64794A65-FAEC-4DDA-BD02-BD49D1E508D4%2F863192D1-8B51-4E4F-8ADA-090F036486DB_2%2FHeMRoxgkVNShNDzuxLGAwU3UNxsHwI2ZCfxDUFQW7R0z%2Fbg.svg&border=none&extra=%20&logo=https%3A%2F%2Fres.craft.do%2Fuser%2Ffull%2Fa00fc09b-5dd0-bc21-aaeb-f7e491dce279%2Fdoc%2F64794A65-FAEC-4DDA-BD02-BD49D1E508D4%2FC3C42ED6-11C7-43D5-9AEE-DBED0A117DB1_2%2Fx9U57uyeF3jYtJyzHNoflQriJyWo1VsSOl5AU6BSgX0z%2Fplanet.svg&ogType=og&rounded=none&theme=dark&title=The%20collection%20of%20CSS%20knowledge'
 
@@ -37,81 +38,7 @@ export default defineConfig({
             link: '/demo/basic/cloud',
           },
         ],
-        sidebar: {
-          '/demo/': [
-            {
-              text: 'Basic',
-              items: [
-                {
-                  text: 'cloud',
-                  link: '/demo/basic/cloud',
-                },
-                {
-                  text: 'dazzle',
-                  link: '/demo/basic/dazzle',
-                },
-                {
-                  text: 'palette',
-                  link: '/demo/basic/palette',
-                },
-                {
-                  text: 'shadow',
-                  link: '/demo/basic/shadow',
-                },
-                {
-                  text: 'gradient text',
-                  link: '/demo/basic/gradient-text',
-                },
-              ],
-            },
-            {
-              text: 'Animation',
-              items: [
-                {
-                  text: 'horse racing light',
-                  link: '/demo/animation/horse-racing-light',
-                },
-                {
-                  text: 'ripple',
-                  link: '/demo/animation/ripple',
-                },
-                {
-                  text: 'spin',
-                  link: '/demo/animation/spin',
-                },
-                {
-                  text: 'tiktok',
-                  link: '/demo/animation/tiktok',
-                },
-                {
-                  text: 'typewriter',
-                  link: '/demo/animation/typewriter',
-                },
-              ],
-            },
-            {
-              text: 'Interact',
-              items: [
-                {
-                  text: 'dynamic background color',
-                  link: '/demo/interact/dynamic-background-color',
-                },
-                {
-                  text: 'dynamic border',
-                  link: '/demo/interact/dynamic-border',
-                },
-                {
-                  text: 'dynamic color',
-                  link: '/demo/interact/dynamic-color',
-                },
-                {
-                  text: 'neon',
-                  link: '/demo/interact/neon',
-                },
-              ],
-            },
-          ],
-        },
+        sidebar: sidebarEn,
       },
     },
     zh: {
@@ -129,81 +56,7 @@ export default defineConfig({
             link: '/zh/demo/basic/cloud',
           },
         ],
-        sidebar: {
-          '/zh/demo/': [
-            {
-              text: '基础',
-              items: [
-                {
-                  text: '云',
-                  link: '/zh/demo/basic/cloud',
-                },
-                {
-                  text: '炫彩',
-                  link: '/zh/demo/basic/dazzle',
-                },
-                {
-                  text: '色盘',
-                  link: '/zh/demo/basic/palette',
-                },
-                {
-                  text: '阴影',
-                  link: '/zh/demo/basic/shadow',
-                },
-                {
-                  text: '渐变文字',
-                  link: '/zh/demo/basic/gradient-text',
-                },
-              ],
-            },
-            {
-              text: '动画效果',
-              items: [
-                {
-                  text: '走马灯',
-                  link: '/zh/demo/animation/horse-racing-light',
-                },
-                {
-                  text: '自旋',
-                  link: '/zh/demo/animation/spin',
-                },
-                {
-                  text: '波纹',
-                  link: '/zh/demo/animation/ripple',
-                },
-                {
-                  text: '抖音',
-                  link: '/zh/demo/animation/tiktok',
-                },
-                {
-                  text: '打字机',
-                  link: '/zh/demo/animation/typewriter',
-                },
-              ],
-            },
-            {
-              text: '交互效果',
-              items: [
-                {
-                  text: '动态背景',
-                  link: '/zh/demo/interact/dynamic-background-color',
-                },
-                {
-                  text: '动态边框',
-                  link: '/zh/demo/interact/dynamic-border',
-                },
-                {
-                  text: '动态颜色',
-                  link: '/zh/demo/interact/dynamic-color',
-                },
-                {
-                  text: '霓虹',
-                  link: '/zh/demo/interact/neon',
-                },
-              ],
-            },
-          ],
-        },
+        sidebar: sidebarZh,
       },
     },
   },
